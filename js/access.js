61eb546c899634814a3d72108d260db906e83237
Apisit Ritreungroj <38898766+kemaoaods@users.noreply.github.com>
2018-10-05 21:04:58 +0700

    UA-002

--- js/access.js
+++ js/access.js
@@ -1,31 +1,35 @@
 ﻿var isLocked = true;
 function noscroll() {
-    if (isLocked === true) {
-        window.scrollTo( 0, 0 );
-    } 
-}
-Locked();
-function Locked() {
-    if (isLocked === false) {
-        document.getElementById('modal').style.display = "none";
-    }
+    if (isLocked === true) window.scrollTo( 0, 0 );
+    else document.getElementById('modal').style.display = "none";
 }
 
 document.getElementById("access").addEventListener("keyup", function(event) {
     event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("accessBtn").click();
-    }
+    if (event.keyCode === 13) document.getElementById("accessBtn").click();
 });
 
 function Password() {
     if (document.getElementById("access").value === "48826") {
         document.getElementById('modal').style.display = "none";
+        document.getElementById('navbar').style.display = "block";
         window.removeEventListener('scroll', noscroll);
     }
     else {
-        document.getElementById("access").value = '';
-        document.getElementById("incorrect").style.display = "block";
+        if ( document.getElementById('modal-content').classList.contains("shakesideplay")) document.getElementById('modal-content').classList.remove("shakesideplay");
+        document.getElementById('access').style.color = "grey";
+        document.getElementById('access').disabled = true;
+        document.getElementById('accessBtn').style.pointerEvents = "none";
+        document.getElementById('accessBtn').style.opacity = "0.75";
+        setTimeout(function() {
+            document.getElementById("access").value = '';
+            document.getElementById('access').style.color = "black";
+            document.getElementById('access').disabled = false;
+            document.getElementById('accessBtn').style.pointerEvents = "auto";
+            document.getElementById('accessBtn').style.opacity = "1";
+            document.getElementById("access").focus();
+            document.getElementById('modal-content').classList.add("shakesideplay");
+        }, 2000);
     }
 }
 
